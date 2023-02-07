db0afc001eb30b7ebf03003d2d72c5061c3c4919
Tugce Guzle <tugce.guzle1928@gmail.com>
2023-02-07 13:43:34 +0300

    add files

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="info">
+        <p>Click to edit a todo</p>
+        <p>
+          Created by <a href="https://github.com/tugceguzle">Tuğçe Güzle</a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
